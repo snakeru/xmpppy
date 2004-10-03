cdb1d018a5f70129560932f7a02939461ae23e0a
Alexey Nezhdanov <snakeru@gmail.com>
2004-10-03 17:46:25 +0000

    Core modules are in-place now

--- xmppd/modules/__init__.py
+++ xmppd/modules/__init__.py
@@ -1,25 +1,29 @@
 # $Id$
 
 import os
-from psyco.classes import *
+#from psyco.classes import *
 
 for m in os.listdir('modules'):
     if m[:2]=='__' or m[-3:]<>'.py': continue
     exec "import "+m[:-3]
 
-"""
-jabberd2 consists mainly of:
-    sm  - XMPP-IM
-    c2s - auth and registration
-    s2s - jabber:server namespace
-"""
-
 addons = [
+# System stuff
     config.Config,
-    stream.TLS,
     db_fake.AUTH,
+    db_fake.DB,
+
+# XMPP-Core
+    stream.TLS,
     stream.SASL,
+
+# XMPP-IM
     stream.Bind,
-    jep0077.IBR,
+    stream.Session,
+#    privacy.Privacy,
+    router.Router,
+
+# JEPs
+#    jep0077.IBR,
     jep0078.NSA,
     ]
