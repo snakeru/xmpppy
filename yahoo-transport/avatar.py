0112838d9cf3ad0aafc42d5fa6855e1cbb15c4b4
Mike Albon <mikea@yuri.org.uk>
2005-06-01 05:58:22 +0000

    Some robustness changes to make avatar retrieval less likely to crash the transport

--- yahoo-transport/avatar.py
+++ yahoo-transport/avatar.py
@@ -3,11 +3,15 @@ import httplib
 def getavatar(aid):
     #the aid value is the Avatar ID. This is given in tag 197
     conn = httplib.HTTPConnection("img1.avatar.vip.dcn.yahoo.com")
+    conn.debuglevel=3
     conn.request("GET","/users/%s.medium.png"%aid)
     try:
         r1 = conn.getresponse()
-    except IOError:
+    except:
         return None
     #print r1.status, r1.reason
     if r1.status == 200:
-        return r1.read()
+	try:
+           return r1.read()
+	except TypeError:
+	   return None
