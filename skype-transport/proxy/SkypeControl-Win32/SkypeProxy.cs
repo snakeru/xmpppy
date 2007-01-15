bbba66c2686a08d33744e41f1e31ba7b8930a4a7
Norman Rasmussen <norman@rasmussen.co.za>
2007-01-15 20:41:03 +0000

    Fixing connect typo

--- skype-transport/proxy/SkypeControl-Win32/SkypeProxy.cs
+++ skype-transport/proxy/SkypeControl-Win32/SkypeProxy.cs
@@ -33,7 +33,7 @@ namespace SkypeControl
 
         private SkypeClient mySkypeClient = new SkypeClient();
 
-        public bool Conect()
+        public bool Connect()
         {
             return mySkypeClient.Connect();
         }
